e41e9883620fd978cc6187b30c28b715841055c3
kushmirc <107213928+kushmirc@users.noreply.github.com>
2023-08-31 16:24:37 -0600

    Modified layout, added "About" page

--- web.py
+++ web.py
@@ -2,6 +2,7 @@ import streamlit as st
 import functions
 
 todos = functions.get_todos()
+#st.set_page_config(layout="wide")
 
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
@@ -12,7 +13,12 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This tool allows you to track to-do tasks")
-st.write("You can Add, Edit, and Complete tasks on the list")
+st.write("You can Add, Edit, and Complete <b>tasks<b> on the list",
+         unsafe_allow_html=True)
+
+st.text_input(label="Enter a todo:", label_visibility="hidden",
+              placeholder="Add a new todo...", on_change=add_todo,
+              key="new_todo")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -22,6 +28,3 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="Enter a todo:", label_visibility="hidden",
-              placeholder="Add a new todo...", on_change=add_todo,
-              key="new_todo")
